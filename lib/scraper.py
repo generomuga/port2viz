9ec557668bca9dcf5f79b7fa2bc81c0f4fd603ce
Gene Romuga <generomuga12693@gmail.com>
2021-09-23 19:53:22 +0800

    Removed selenium library

--- lib/scraper.py
+++ lib/scraper.py
@@ -1,12 +1,4 @@
-from selenium import webdriver
-from selenium.webdriver.common import keys
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
 from bs4 import BeautifulSoup
-
 import requests
 
 class Scraper:
