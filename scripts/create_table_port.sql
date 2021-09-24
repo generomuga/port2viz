c5ca208a27f937bddd11e3ef82c05bb2f22c252d
Gene Romuga <generomuga12693@gmail.com>
2021-09-24 08:53:27 +0800

    Updated script

--- scripts/create_table_port.sql
+++ scripts/create_table_port.sql
@@ -1,8 +1,9 @@
-CREATE TABLE port (
+CREATE TABLE IF NOT EXISTS port (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	port_name text,
 	country_name text,
 	unlocode text,
 	function text,
-	coordinates text
+	coordinates text,
+	is_failed_mapping int
 )
